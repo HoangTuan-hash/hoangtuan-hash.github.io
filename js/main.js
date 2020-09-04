6a3338ee54d09d4e55b768b2da5ed6c0994ed033
HoangTuan-hash <hoangtuan20026@gmail.com>
2020-09-04 10:41:51 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var ticket = document.querySelectorAll(".ticket__item");
+var dropDow = document.querySelectorAll(".ticket__dropdow");
+for (let i = 0; i < ticket.length; i++) {
+  ticket[i].addEventListener("click", function () {
+    var index = i;
+    dropDow[i].classList.toggle("ticket__show--drop");
+    for (let j = 0; j < dropDow.length; j++) {
+      if (j != index) {
+        dropDow[j].classList.remove("ticket__show--drop");
+      }
+    }
+    document.addEventListener('mouseup', function(e) {
+      var ticket = document.getElementById('ticket');
+      if (!ticket.contains(e.target)) {
+        dropDow[i].classList.remove("ticket__show--drop");
+      }
+    });
+  });
+  
+}
